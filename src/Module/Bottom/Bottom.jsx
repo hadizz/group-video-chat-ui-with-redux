4faa69470d40c66cb48b94e2d416dc604d4568c4
hadizz <hadizareoriginal@gmail.com>
2021-02-26 00:54:34 +0330

    feat: Implement button component

--- src/Module/Bottom/Bottom.jsx
+++ src/Module/Bottom/Bottom.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './Bottom.module.css'
+import IconButton from '../Components/IconButton/IconButton'
 
 const Bottom = () => {
-  return <div className={styles.root}>bottom</div>
+  return (
+    <div className={styles.root}>
+      bottom
+      <IconButton iconName="call" size="small" color="red50" iconColor='red' onClick={e => console.log(e)}/>
+      <IconButton iconName="call" size="medium" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="call" size="large" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="no-voice" size="small" color="red50" iconColor='red' onClick={e => console.log(e)}/>
+      <IconButton iconName="no-voice" size="medium" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="no-voice" size="large" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="no-video" size="small" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="no-video" size="medium" color="red" onClick={e => console.log(e)}/>
+      <IconButton iconName="no-video" size="large" color="red" onClick={e => console.log(e)}/>
+    </div>
+  )
 }
 
 export default Bottom
---
+++ src/Module/Components/IconButton/IconButton.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styles from './IconButton.module.css'
+import generateClassName from '../../../Utils/dom'
+import { NoVideoIcon, NoVoiceIcon, CallIcon } from './Icons'
+
+const IconButton = ({ iconName, size, color, iconColor, onClick }) => {
+
+  const generateSize = () => {
+    switch (size) {
+      case 'small': return 20;
+      case 'large': return 30;
+      default:
+      case 'medium': return 25;
+    }
+  }
+
+  const generateIcon = (iconColor = 'white') => {
+    const iconSize = generateSize();
+    const color = iconColor === 'red' ? "#EA4141" : iconColor;
+    switch (iconName) {
+      case 'voice':
+        return <NoVideoIcon fill={color} size={iconSize} />
+      case 'no-voice':
+        return <NoVoiceIcon fill={color} size={iconSize} />
+      default:
+      case 'call':
+        return <CallIcon fill={color} size={iconSize} />
+    }
+  }
+
+  const handleOnClick = (ev) => {
+    if (onClick) {
+      onClick(ev)
+    }
+  }
+
+  return (
+    <button
+      onClick={handleOnClick}
+      className={generateClassName([
+        styles.root,
+        styles[`size-${size ?? 'medium'}`],
+        styles[`bgColor-${color ?? 'default'}`],
+      ])}
+    >
+      {generateIcon(iconColor)}
+    </button>
+  )
+}
+
+export default IconButton
