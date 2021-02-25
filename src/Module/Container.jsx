ac9a1d3862a87ee94818a34c3999c7d60a979dd8
hadizz <hadizareoriginal@gmail.com>
2021-02-25 23:28:48 +0330

    chore: change folder name

--- src/Module/Container.jsx
+++ src/Module/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header/Header'
 import CallsList from './CallsList/CallsList'
 import Bottom from './Bottom/Bottom'
-import generateClassName from '../utils/dom'
+import generateClassName from '../Utils/dom'
 import styles from './Container.module.css'
 
 const Container = () => {
